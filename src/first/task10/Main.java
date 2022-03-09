dd9719f7003a97d059726a4fcb2758d4e150779f
Oksana Topol <aksana.topal@gmail.com>
2022-03-09 15:43:15 +0300

    Third part of first homework

--- src/first/task10/Main.java
+++ src/first/task10/Main.java
@@ -1,10 +1,28 @@
 package first.task10;
 
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
+        int e = 5, f = 6, a = 1, b = 6, c = 1, d = 1;
 
+        if ((e >= (a + c)) && (f >= b && f >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (a + d)) && (f >= b && f >= c)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (b + c)) && (f >= a && f >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (b + d)) && (f >= a && f >= c)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (a + c)) && (e >= b && e >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (a + d)) && (e >= b && e >= c)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (b + c)) && (e >= a && e >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (b + d)) && (e >= a && e >= c)) {
+            System.out.println("Можно построить 2 дома");
+        }
+        else
+            System.out.println("Нельзя построить 2 дома");
     }
-    public static boolean vpihnytDoma(double )
-
 }
--- src/first/task9/Main.java
+++ src/first/task9/Main.java
@@ -3,13 +3,69 @@ package first.task9;
 public class Main {
 
     public static void main(String[] args) {
-
-    }
-    public static void nextDay(int day, int month, int year){
-        if (day >= 1 || day <=31){
-            day++;
+        int year, month, day;
+        year = 2020;
+        month = 2;
+        day = 29;
+        if (month == 1 || month == 3 || month == 5 || month == 1 || month == 7
+                || month == 8 || month == 10) {
+            if (!(1 > day) && !(day > 30)) {
+                day++;
+                System.out.println(year + "." + month + "." + day);
+            } else if (day == 31) {
+                day = 1;
+                month++;
+                System.out.println(year + "." + month + "." + day);
+            }
+        } else if (month == 2) {
+            if (!(0 > year) && !(year > 3000))
+                if (year % 400 == 0) {
+                    if (!(1 > day) && !(day > 28)) {
+                        day++;
+                        System.out.println(year + "." + month + "." + day);
+                    } else if (day == 29) {
+                        day = 1;
+                        month++;
+                        System.out.println(year + "." + month + "." + day);
+                    }
+                } else if (year % 4 == 0 && year % 100 != 0) {
+                    if (!(1 > day) && !(day > 28)) {
+                        day++;
+                        System.out.println(year + "." + month + "." + day);
+                    } else if (day == 29) {
+                        day = 1;
+                        month++;
+                        System.out.println(year + "." + month + "." + day);
+                    }
+                } else {
+                    if (!(1 > day) && !(day > 27)) {
+                        day++;
+                        System.out.println(year + "." + month + "." + day);
+                    } else if (day == 28) {
+                        day = 1;
+                        month++;
+                        System.out.println(year + "." + month + "." + day);
+                    }
+                }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (!(1 > day) && !(day > 29)) {
+                day++;
+                System.out.println(year + "." + month + "." + day);
+            } else if (day == 30) {
+                day = 1;
+                month++;
+                System.out.println(year + "." + month + "." + day);
+            }
+        } else if (month == 12) {
+            if (!(1 > day) && !(day > 30)) {
+                day++;
+                System.out.println(year + "." + month + "." + day);
+            } else if (day == 31) {
+                day = 1;
+                year++;
+                month = 1;
+                System.out.println(year + "." + month + "." + day);
+            }
         }
-
-
     }
 }
